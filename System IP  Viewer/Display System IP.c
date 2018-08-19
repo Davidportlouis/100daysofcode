0476c18a54d3578521c4b71111272cd9960d8235
David Port Louis <18904157+Davidportlouis@users.noreply.github.com>
2018-08-19 11:29:06 +0530

    This is Used For Viewing IP
    This runs the ipconfig  in System32 folder and retunrs the output

---
+++ System IP  Viewer/Display System IP.c
@@ -0,0 +1,25 @@
+/* This C Program Prints The IP Address Of You Computer using
+   system() Function*/
+
+///////////// Header Files /////////////////
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    system("C:\\Windows\\System32\\ipconfig"); // This Access The ipconfig command and displays the Data
+    getchar();
+}
+
+/*
+**********************************************************************************
+**********************************************************************************
+**Title:IP Address Viewer                                                       **
+**URL: https://github.com/Davidportlouis/Source-Code-For-Simple-C-Program       **
+**Date_start: 19/08/2018                                                        **
+**Author: David Port Louis                                                      **
+**My Special Thanks To Bucky Roberts I Learned C From his Tutorial Thanks Bucky!**
+**********************************************************************************
+**********************************************************************************
+*/
