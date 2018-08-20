4981b32d2787356f5d1c18f537708485adace030
David Port Louis <18904157+Davidportlouis@users.noreply.github.com>
2018-08-20 18:54:30 +0530

    Call Erros Corrected Code Working !
    Missed a , in scanf() Statement now corrected the code

--- Find Roots of Quad Equ/Roots of Quad Equ.c
+++ Find Roots of Quad Equ/Roots of Quad Equ.c
@@ -11,22 +11,22 @@ int main()
     double a,b,c,det,root1,root2,real,imaginary;
 
     printf("Enter Coefficients of a, b, c : \n");
-    scanf("%lf %lf %lf,&a,&b,&c");
-    det=b*b-4*a*c;
-    if(det==0)
-    {
-        root1 = root2 = -b/(2*a);
-        printf("root1 = %.2lf & root2 = %.2lf",root1,root2);
-    }else if(det>0)
+    scanf("%lf %lf %lf",&a,&b,&c);
+    det=(b*b)-(4*a*c);
+    if(det>0)
     {
         root1 = (-b+sqrt(det))/(2*a);
         root2 = (-b-sqrt(det))/(2*a);
-        printf("root1 = %.2lf and root2 = %.2lf",root1 , root2);
+        printf("root1:%.2lf and root2:%.2lf",root1,root2);
+    }else if(det==0)
+    {
+        root1 = root2 = -b/(2*a);
+        printf("root1 = %.2lf and root2 = %.2lf",root1,root2);
     }else
     {
         real=-b/(2*a);
         imaginary=sqrt(-det)/(2*a);
-        printf("root1 =%.2lf+i%.2lf & %.2lf+i%.2lf",real,imaginary,real,imaginary);
+        printf("root1:%.2lf+i%.2lf and root2:%.2lf+i%.2lf",real,imaginary,real,imaginary);
     }
     getch();
 }
