3b11ce8b97a351cd511062a87f83dc6a86719bf0
David Port Louis <dportlouis@gmail.com>
2020-01-09 17:49:38 +0530

    day1 of 100

---
+++ d1_hello.c
@@ -0,0 +1,13 @@
+// "Hello World!" in C
+// Hackerrank C introduction #100daysofcode day-1
+
+#include<stdio.h>
+
+int main(void)
+{
+    char *input[100];
+    scanf("%s",input);
+    printf("Hello, World!\n");
+    printf("%s\n",input);
+    return 0;
+}
