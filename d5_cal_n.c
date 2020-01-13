a3f9a4d4b118452a161470e56964ac74621104d8
David Port Louis <dportlouis@gmail.com>
2020-01-13 18:33:49 +0530

    cal_n_term

--- d5_cal_n.c
+++ d5_cal_n.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
-#include<math.h>
+#include<stdlib.h>
 
-int find_nth_term(int a,int b,int c,int n)
+int find_n_th_term(int n,int a,int b,int c)
 {
-    
+    int s[20];
+    s[1] = a,s[2]=b,s[3]=c;
+    for(int i=4;i<=n;i++)
+    {
+        s[i] = s[i-3] + s[i-2] + s[i-1];
+    }
+    return s[n];
 }
+
 
 int main(void)
 {
     int n,a,b,c;
     scanf("%d %d %d %d",&n,&a,&b,&c);
-    int ans = find_nth_term(a,b,c,n);
+    int ans = find_n_th_term(n,a,b,c);
     printf("%d",ans);
     return 0;
 }
