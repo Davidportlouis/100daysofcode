e8db58cf61383b2b20fb052eef65ea34cc8a6a52
David Port Louis <dportlouis@gmail.com>
2020-01-14 10:13:11 +0530

    day 6

---
+++ d6_daily_challenge.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+char* getStringOfABC(int N)
+{
+    char *str1 = malloc(N*sizeof(char));
+    int count = 0;
+    while(count<N)
+    {
+    for(char c='a';c<'d'&&count<N;c++)
+        {
+            str1[count] +=c;
+            count++;
+        }
+    }
+}
+
+int main(void)
+{
+    int N;
+    scanf("%d",&N);
+    getStringOfABC(N);
+    char *str = getStringOfABC(N);
+    // printf(str);
+    free(str);
+    return 0;
+}
