61db062e718cc9b9c6773699fba90c178b5f422c
David Port Louis <dportlouis@gmail.com>
2020-02-22 19:16:23 +0530

    day 43 of 100

--- d42_linked_list.c
+++ d42_linked_list.c
@@ -32,3 +32,39 @@ list * create_list()
     return l;
 }
 
+// front operations
+void insert_front(list *l,int val)
+{
+    node *n = (node *)malloc(sizeof(node));
+    n->data = val;
+    n->link = l->head;
+    l->head = n;
+    l->size++;
+}
+
+int delete_front(list *l)
+{
+    if(is_empty(l))
+    {
+        printf("List Empty\n");
+        return 0;
+    }
+    int val = l->head->data;
+    node *temp = l->head;
+    l->head = l->head->link;
+    free(temp);
+    l->size--;
+    return val;
+}
+
+int main(void)
+{
+    list *l = create_list();
+    insert_front(l,10);
+    insert_front(l,20);
+    while(!is_empty(l))
+    {
+        printf("%d\n",delete_front(l));
+    }
+    return 0;
+}
