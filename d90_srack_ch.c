8d9569e26ab6af54689ba0d94de83158d2268a7e
davidportlouis <dportlouis@gmail.com>
2020-06-02 07:57:11 +0530

    modified d90

--- d90_srack_ch.c
+++ d90_srack_ch.c
@@ -18,24 +18,17 @@ void sort(int *arr,int n)
 
 int main(void)
 {
-    char s[100];
-    int n, alpha[26]={0},digit[10]={},j=0,a_max,d_max;
+    char s[1000];
+    int n,chars[60]={0},j,max;
     scanf("%s%n",s,&n);
-    for(int i=0;i<n;i++)
-    {
-        if(s[i] >= 'a' && s[i] <= 'z'){
-            j = s[i] - 'a';
-            // printf("%d ",j);
-            ++alpha[j];
-        }else if(s[i] >= '0' && s[i] <= '9'){
-            j = s[i] - '0';
-            ++digit[j];
+    for(int i=0;i<n;i++){
+        if(s[i] >= 'A' || s[i] <= 'z'){
+            j = s[i] - 'A';
+            ++chars[j];
         }
     }
-    sort(alpha,26);
-    sort(digit,10);
-    a_max = alpha[0];
-    d_max = digit[0];
-    (a_max > d_max) ? printf("%d\n",a_max) : printf("%d\n",d_max);
+    sort(chars,60);
+    max = chars[0];
+    printf("%d\n",max);
     return 0;
 }
