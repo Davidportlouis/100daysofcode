3730f950011049a233d839a508b65d91dfad3c73
davidportlouis <dportlouis@gmail.com>
2020-06-02 08:38:46 +0530

    output success

--- d90_srack_ch.c
+++ d90_srack_ch.c
@@ -19,15 +19,16 @@ void sort(int *arr,int n)
 int main(void)
 {
     char s[1000];
-    int n,chars[60]={0},j,max;
+    int n,chars[74]={0},j,max;
     scanf("%s%n",s,&n);
     for(int i=0;i<n;i++){
-        if(s[i] >= 'A' || s[i] <= 'z'){
-            j = s[i] - 'A';
+        if(s[i] >= '0' || s[i] <= 'z'){
+            j = s[i] - '0';
+            // printf("%d ",j);
             ++chars[j];
         }
     }
-    sort(chars,60);
+    sort(chars,74);
     max = chars[0];
     printf("%d\n",max);
     return 0;
