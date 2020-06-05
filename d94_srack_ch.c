68c98cc4613125c0784386b7debaf1dea9f68677
davidportlouis <dportlouis@gmail.com>
2020-06-06 05:18:26 +0530

    day 94 of 100

---
+++ d94_srack_ch.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int d,m,y;
+    scanf("%d-%d-%d",&d,&m,&y);
+    switch(m)
+    {
+        case 1: printf("January");break;
+        case 2: printf("February");break;
+        case 3: printf("March");break;
+        case 4: printf("April");break;
+        case 5: printf("May"); break;
+        case 6: printf("June");break;
+        case 7: printf("July");break;
+        case 8: printf("August");break;
+        case 9: printf("Spetember");break;
+        case 10: printf("October");break;
+        case 11: printf("November");break;
+        case 12: printf("December");break;
+        default: printf("Invalid");
+    }
+    return 0;
+}
