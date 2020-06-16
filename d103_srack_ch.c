ec7ca586d3a94f97f06b772505541ac8c10e1370
davidportlouis <dportlouis@gmail.com>
2020-06-16 12:17:32 +0530

    day 103 ic

---
+++ d103_srack_ch.c
@@ -0,0 +1,55 @@
+//Rotate Digits - Repeat Pattern
+#include<stdio.h>
+#define MAX 8
+
+static int digits[MAX];
+static int cpy_digits[MAX];
+
+void rotate_clockwise(int n)
+{
+     int temp = cpy_digits[0];
+     for(int i=0;i<n;i++)
+     {
+         cpy_digits[i] = cpy_digits[i+1];
+     }
+     cpy_digits[n-1] = temp;
+}
+
+void extract_digit(int x,int n)
+{
+    int i = n-1;
+    while(x != 0)
+    {
+        digits[i] = x % 10;
+        x/=10;
+        i--;
+    }
+    for(i=0;i<n;i++)
+    {
+        cpy_digits[i] = digits[i];
+    }
+}
+
+int main(void)
+{
+    int n,x;
+    scanf("%d%n",&x,&n);
+    extract_digit(x,n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",cpy_digits[i]);
+    }
+    printf("\n");
+    rotate_clockwise(n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",cpy_digits[i]);
+    }
+    printf("\n");
+    rotate_clockwise(n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",cpy_digits[i]);
+    }
+    return 0;
+}
