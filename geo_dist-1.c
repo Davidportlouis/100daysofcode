6d6a455803e59b229d51eac69facd0b3bc2802f9
davidportlouis <dportlouis@gmail.com>
2020-06-17 23:49:32 +0530

    added geometric dist hackerrank

---
+++ geo_dist-1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+double power(double b,double e)
+{
+    double ans = 1.0;
+    for(int i=0;i<e;i++)
+    {
+        ans *= b;
+    }
+    return ans;
+}
+
+double geo_dist(int x,int n,double p)
+{
+    return power(1.0-p,n-x) * p;
+}
+
+int main(void)
+{
+    int n = 5;
+    double p = 1.0/3.0;
+    printf("%0.3lf\n",geo_dist(i,n,p));
+    return 0;
+}
