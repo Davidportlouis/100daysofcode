9419e58f7312e8d86b1d7ede2bd02e24f336449d
davidportlouis <dportlouis@gmail.com>
2020-06-17 23:50:46 +0530

    added normal dist hackerrank

---
+++ normal_1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<math.h>
+#define e 2.718281828
+#define pi 3.14159265358979
+
+static double mu = 20.0,sigma = 2.0;
+
+
+double power(double base,double exp)
+{
+    double ans = 1;
+    if(exp > 0)
+    {
+        for(int i=0;i<exp;i++)
+        {
+            ans *= base;
+        }
+    }else{
+        for(int i=0;i>exp;i--)
+        {
+            ans /= base;
+        }
+    }
+    return ans;
+}
+
+double sqroot(int x)
+{
+    double temp,ans;
+    ans = x/2;
+    temp = 0;
+    while(ans != temp)
+    {
+        temp = ans;
+        ans = (x/temp + temp)/2;
+    }
+    return ans;
+}
+
+double phi(double x)
+{
+    return 0.5*(1 + erf((x-mu)/(sigma*sqroot(2))));
+}
+
+int main(void)
+{
+    double dis_1,dis_2;
+    dis_1 = phi(19.5);
+    dis_2 = phi(22.0) - phi(20.0);
+    printf("%0.3lf\n",dis_1);
+    printf("%0.3lf\n",dis_2);
+    return 0;
+}
